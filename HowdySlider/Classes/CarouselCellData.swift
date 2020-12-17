dceecc5f5e0f9cea07a7ff25029f97badea8a641
radohecko <radoslavhecko@gmail.com>
2020-12-17 18:23:19 +0100

    Custom cell data

---
+++ HowdySlider/Classes/CarouselCellData.swift
@@ -0,0 +1,14 @@
+//
+//  CarouselCellData.swift
+//  HowdySlider
+//
+//  Created by Rado Hečko on 17/12/2020.
+//
+
+import UIKit
+
+public struct CarouselCellData {
+    var color: UIColor = .white
+    var shadow: Bool = true
+    var radius: CGFloat = 12.0
+}
