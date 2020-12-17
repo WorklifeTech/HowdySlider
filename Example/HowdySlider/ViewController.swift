c16bbb9567407ff6f50299887528fb1fe291d0f4
radohecko <radoslavhecko@gmail.com>
2020-12-17 18:25:26 +0100

    Carousel usage example

--- Example/HowdySlider/ViewController.swift
+++ Example/HowdySlider/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import HowdySlider
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let carousel = Carousel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width / 2))
+        
+        carousel.numberOfPages = 3
+        carousel.cellCornerRadius = 12.0
+        carousel.cellBackgroundColor = "#ffffff"
+        carousel.cellEnableShadow = true
+        carousel.enableAutoPlay = true
+        carousel.enableOneDirectionScroll = false
+        
+        carousel.setupView()
+        
+        view.addSubview(carousel)
     }
 
     override func didReceiveMemoryWarning() {
