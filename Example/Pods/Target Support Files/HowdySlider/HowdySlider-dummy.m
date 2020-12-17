a4bfc98336cd9f42c60b449273032f42b83c76d5
radohecko <radoslavhecko@gmail.com>
2020-12-17 16:07:32 +0100

    Cocoapod library template

---
+++ Example/Pods/Target Support Files/HowdySlider/HowdySlider-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HowdySlider : NSObject
+@end
+@implementation PodsDummy_HowdySlider
+@end
---
+++ Example/Pods/Target Support Files/Pods-HowdySlider_Example/Pods-HowdySlider_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HowdySlider_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HowdySlider_Example
+@end
