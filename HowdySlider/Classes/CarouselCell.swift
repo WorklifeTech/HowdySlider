f8557fa01d24007a5503e8d40a3ad6072960ae72
radohecko <radoslavhecko@gmail.com>
2020-12-17 18:22:58 +0100

    Custom UICollectionView cell

---
+++ HowdySlider/Classes/CarouselCell.swift
@@ -0,0 +1,59 @@
+//
+//  CarouselCell.swift
+//  HowdySlider
+//
+//  Created by Rado Hečko on 17/12/2020.
+//
+
+import UIKit
+
+class CarouselCell: UICollectionViewCell {
+    
+    var data: CarouselCellData? {
+        didSet {
+            guard let data = data else { return }
+            mainView.backgroundColor = data.color
+            mainView.layer.cornerRadius = data.radius
+            
+            if !data.shadow {
+                contentView.addShadow(offset: CGSize.init(width: 0, height: 0),
+                                      color: UIColor.clear,
+                                      radius: 0,
+                                      opacity: 0)
+            }
+        }
+    }
+    
+    fileprivate let mainView: UIView = {
+        let uiView = UIView()
+        
+        uiView.translatesAutoresizingMaskIntoConstraints = false
+        uiView.layer.masksToBounds = true
+        
+        return uiView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        contentView.addSubview(mainView)
+        contentView.layer.masksToBounds = false
+        contentView.addShadow(offset: CGSize.init(width: mainView.frame.width, height: mainView.frame.height),
+                              color: UIColor.black,
+                              radius: 6.0,
+                              opacity: 0.65)
+        
+        mainView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        mainView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10).isActive = true
+        mainView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10).isActive = true
+        mainView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    public func getMainView() -> UIView {
+        return mainView
+    }
+}
