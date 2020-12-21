0fbca0ee5c11ec30312b7fe56cd40b8a99d9b1e3
radohecko <radoslavhecko@gmail.com>
2020-12-21 11:42:12 +0100

    New demo position

--- Example/HowdySlider/ViewController.swift
+++ Example/HowdySlider/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let carousel = Carousel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width / 2))
+        let carousel = Carousel(frame: CGRect(x: 10, y: 60, width: view.frame.width - 20, height: view.frame.width / 2))
         
         carousel.numberOfPages = 3
         carousel.cellCornerRadius = 12.0
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
         carousel.cellEnableShadow = true
         carousel.enableAutoPlay = true
         carousel.enableOneDirectionScroll = false
+        carousel.enablePageControl = true
         
         carousel.setupView()
         
