2ac7c5bfa9f0d6a5e89433067c802c788cbd2e7d
radohecko <radoslavhecko@gmail.com>
2021-04-20 13:14:52 +0200

    Update carousel example

--- Example/HowdySlider/ViewController.swift
+++ Example/HowdySlider/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         
         let carousel = Carousel(frame: CGRect(x: 10, y: 60, width: view.frame.width - 20, height: view.frame.width / 2))
         
-        carousel.numberOfPages = 3
+        carousel.numberOfPages = 5
         carousel.cellCornerRadius = 12.0
         carousel.cellBackgroundColor = "#ffffff"
         carousel.cellEnableShadow = true
@@ -26,6 +26,23 @@ class ViewController: UIViewController {
         carousel.enablePageControl = true
         
         carousel.setupView()
+        
+        let view1 = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width - 20, height: view.frame.width / 2))
+        view1.backgroundColor = .red
+        
+        let view2 = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width - 20, height: view.frame.width / 2))
+        view2.backgroundColor = .blue
+        
+        let view3 = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width - 20, height: view.frame.width / 2))
+        view3.backgroundColor = .yellow
+        
+        let view4 = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width - 20, height: view.frame.width / 2))
+        view4.backgroundColor = .gray
+        
+        let view5 = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width - 20, height: view.frame.width / 2))
+        view5.backgroundColor = .green
+        
+        carousel.setItems(items: [view1, view2, view3, view4, view5])
         
         view.addSubview(carousel)
     }
